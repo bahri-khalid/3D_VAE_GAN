2208c11e9bc6a7726fa8289c65beca68993fea35
bahri khalid <bahri.khalid.101@gmail.com>
2022-08-31 19:32:54 +0100

    reconstruction of the project structur using cookiecutter data science frame wrok

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is all about using the generative adversarial networks architecture with the VAE neural network to generate new 3D data samples using TensorFlow',
+    author='bahri khalid',
+    license='MIT',
+)
